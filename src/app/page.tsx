238f9edf5c6cbb6c5eb6b37d9fbf5ddce6fa842f
Ayden <futanaristic@gmail.com>
2025-01-11 15:48:14 +0900

    feat: Init and migrate db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockData = [
   "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
@@ -54,10 +55,17 @@ const mockData = [
 const mockImage = mockData.map((url, index) => ({ id: index + 1, url }));
 
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+          { posts.map((post) => (
+            <div key={post.id + 100} className="w-48">
+              { post.name }
+            </div>
+          ))}
           {
             mockImage.map((image) => (
               <div key={image.id} className="w-48">
